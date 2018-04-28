dde14da38e6cddf59fa1d4ebff2ff85dfa6e8e3e
Carl Egbert <egbertcarl@gmail.com>
2018-04-28 15:17:41 -0700

    get secret validation working

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, make_response, redirect, request, url_for
+from flask import Flask, abort, make_response, redirect, request, url_for
 import hmac
 from hashlib import sha1
 from os import environ
 
 
-secret = environ['CROCODILE_SECRET']
+secret = environ['CROCODILE_SECRET'].encode('utf-8')
 
 app = Flask(__name__)
 
 
-def check_signature(request):
-    data = request.data
-    signature = request.headers.get('X-Hub-Signature').split('=')[1]
+def check_signature(req):
+    data = req.data
+    signature = req.headers.get('X-Hub-Signature').split('=')[1]
     digest = hmac.new(secret, data, sha1).hexdigest()
     return hmac.compare_digest(signature, digest)
 
@@ -22,9 +22,9 @@ def index():
         return redirect(url_for('dashboard'))
 
     if check_signature(request):
-        make_response(200)
+        return make_response('Accepted', 200)
 
-    return make_response(404)
+    abort(404)
 
 
 @app.route('/dashboard', methods=['GET'])
