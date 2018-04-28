24f498c3f5a78679ff7ef67e98ba1685efc83169
Carl Egbert <egbertcarl@gmail.com>
2018-04-28 13:19:42 -0700

    first attempt at secret validation

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, make_response, redirect, request, url_for
+import hmac
+from hashlib import sha1
+from os import environ
+
+
+secret = environ['CROCODILE_SECRET']
 
 app = Flask(__name__)
+
+
+def check_signature(request):
+    data = request.data
+    signature = request.headers.get('X-Hub-Signature').split('=')[1]
+    digest = hmac.new(secret, data, sha1).hexdigest()
+    return hmac.compare_digest(signature, digest)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -8,7 +21,10 @@ def index():
     if request.method == 'GET':
         return redirect(url_for('dashboard'))
 
-    return make_response(200)
+    if check_signature(request):
+        make_response(200)
+
+    return make_response(404)
 
 
 @app.route('/dashboard', methods=['GET'])
