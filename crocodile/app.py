bfc59dfc91eebcd0459860cb6ddec24e276d7e4a
Carl Egbert <egbertcarl@gmail.com>
2018-05-04 22:30:54 -0700

    make /build kick off build script

--- crocodile/app.py
+++ crocodile/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, make_response, request
+from flask import Flask, abort, make_response, request
+from subprocess import Popen
 
-from crocodile import auth, hooks
+from crocodile import auth
+from crocodile.hooks import hooks
 
 
 app = Flask(__name__)
@@ -14,4 +16,10 @@ def index():
 @app.route('/build', methods=['POST'])
 @auth.signature_required
 def build():
-    return 'wahee'
+    hook_name = request.headers['X-GitHub-Event']
+    hook = hooks.get(hook_name)
+    if not hook:
+        abort(404)
+
+    Popen(hook, shell=True)
+    return make_response('success', 200)
