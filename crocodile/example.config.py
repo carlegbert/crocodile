5702fe68570b0c89a09138a999cf3897def0309e
Carl Egbert <egbertcarl@gmail.com>
2018-05-15 21:02:48 -0700

    readme

--- crocodile/example.config.py
+++ crocodile/example.config.py
@@ -1,2 +1,11 @@
+from os import path
+
+
+REDIS_URL = 'redis://localhost:6379/0'  # or other address for redis
 CROCODILE_SECRET = 'secret_goes_here'
 LOGFILE = 'crocodile.log'
+
+CONSUMERSFILE = path.join(path.dirname(__file__), 'consumers.yml')
+BROKER_TRANSPORT_OPTIONS = {
+    'visibility_timeout': 3600
+}
