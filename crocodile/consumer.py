8a37e4e9234e42cd6be1fcbb74e73291d55b3526
Carl Egbert <egbertcarl@gmail.com>
2018-05-27 11:31:16 -0700

    first attempt at email notifications

--- crocodile/consumer.py
+++ crocodile/consumer.py
@@ -1,25 +1,31 @@
+from datetime import datetime
 from flask import current_app, request
 import yaml
 
-from crocodile.tasks import build
+from crocodile.tasks import build, send_notification_email
 
 
 class Consumer(object):
 
     _consumer_list = []
 
-    def __init__(self, event_type, ref, action):
+    def __init__(self, name, event_type, ref, action, watchers):
+        self.name = name
         self.event_type = event_type
         self.ref = ref
         self.action = action
+        self.watchers = watchers
 
     def run(self):
         if current_app.config['TESTING']:
             return
 
-        current_app.logger.info('Build initiated for %s:%s:%s'
-                                % (self.event_type, self.ref, self.action))
+        current_app.logger.info('Build initiated for %s:%s:%s:%s'
+                                % (self.name, self.event_type, self.ref,
+                                   self.action))
+        self._notify_build_started()
         build.delay(self.to_dict())
+        build.delay(self)
 
     def to_dict(self):
         return {
@@ -27,6 +33,14 @@ class Consumer(object):
             'ref': self.ref,
             'action': self.action
         }
+
+    def _notify_build_started(self):
+        time = datetime.now()
+        subject = 'Build started for %s' % self.name
+        message = 'Build was triggered at %s for application %s due to %s'
+        'event for branch %s' % (time, self.name, self.event_type,
+                                 self.ref)
+        send_notification_email(self.watchers, subject, message)
 
     @classmethod
     def from_dict(cls, d):
--- crocodile/tasks.py
+++ crocodile/tasks.py
@@ -1,6 +1,11 @@
+import smtplib
 import subprocess
+from socket import gethostname
 from celery import Celery
 from crocodile import config
+
+_HOSTNAME = gethostname()
+_FROM = 'crocodile %s' % _HOSTNAME
 
 
 celery = Celery(
@@ -12,3 +17,10 @@ celery = Celery(
 @celery.task()
 def build(consumer):
     subprocess.run(consumer.get('action'), shell=True)
+
+
+@celery.task()
+def send_notification_email(recipients, subject, message):
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(_FROM, recipients, message)
+    smtp.close()
